115a533d28b1ff22d9e67b2a10af50beea7750e2
labsfiap <manciliaotavio0@gmail.com>
2026-03-18 20:48:03 -0300

    aula 2 variaveis

---
+++ aula02-variaveis-operadores/aula02A-variaveis.py
@@ -0,0 +1,24 @@
+print("ola mundo")
+
+print(7+4)
+
+#variáveis
+nome="Otavio" # str
+idade=18
+peso=80.7
+
+print("oiii",nome, idade, peso)
+print(f"oii {nome}!")
+
+#imput - simulação de um forms no cmd
+nome = input("Digite seu nome: ")
+print(f"Boas-vindas {nome}")
+dia_que_nasceu = input("Digite o dia que você nasceu: ")
+mes_que_nasceu = input("Digite o mes que você nasceu: ")
+ano_que_nasceu = input("Digite o ano que você nasceu: ")
+print(f"Você nasceu dia {dia_que_nasceu} do mes {mes_que_nasceu} do ano {ano_que_nasceu}")
+
+numero_1 = int(input("Digite um numero: "))
+numero_2 = int(input("Digite outro numero: "))
+print(numero_1+numero_2)
+
